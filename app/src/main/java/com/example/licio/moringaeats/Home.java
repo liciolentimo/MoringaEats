65dc2316c6d14b9fb0e7790fc7878c2d2425ac86
liciolentimo <lentimo@gmail.com>
2019-03-01 12:27:57 +0300

    Added toast message when item in array is clicked

--- app/src/main/java/com/example/licio/moringaeats/Home.java
+++ app/src/main/java/com/example/licio/moringaeats/Home.java
@@ -1,9 +1,14 @@
 package com.example.licio.moringaeats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,6 +20,8 @@ public class Home extends AppCompatActivity {
 
     @BindView(R.id.listView)
     ListView mListView;
+    @BindView(R.id.txtRecipe)
+    TextView mTxtRecipe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +31,17 @@ public class Home extends AppCompatActivity {
 
         final RecipesAdapter adapter = new RecipesAdapter(this, R.layout.support_simple_spinner_dropdown_item,recipes);
         mListView.setAdapter(adapter);
+
+                mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String recipe = ((TextView)view).getText().toString();
+                Toast.makeText(Home.this, recipe, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        mTxtRecipe.setText("The following recipes are loved by "+ name);
     }
 }
