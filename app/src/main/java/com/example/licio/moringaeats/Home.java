612e9856908ec17d3dc76c9abe0cb74568eb8b39
liciolentimo <lentimo@gmail.com>
2019-03-01 12:13:15 +0300

    Added recipes array and bindview using butterknife

--- app/src/main/java/com/example/licio/moringaeats/Home.java
+++ app/src/main/java/com/example/licio/moringaeats/Home.java
@@ -2,6 +2,7 @@ package com.example.licio.moringaeats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import butterknife.BindView;
@@ -9,7 +10,8 @@ import butterknife.ButterKnife;
 
 public class Home extends AppCompatActivity {
     private String[] recipes = new String[] {"Sweet Potatoes with Apple Butter","Old-Fashioned Apple Pie","Beef Stew in Red Wine Sauce",
-    "Butternut Squash Soup with Crisp Pancetta","Hot Mulled Cider","Pear-Cranberry Hand Pies"};
+    "Butternut Squash Soup with Crisp Pancetta","Hot Mulled Cider","Pear-Cranberry Hand Pies","Caramel Lady Apples","Three-Chile Beef Chili",
+    "Poached Egg over Spinach and Mushroom","10-Minute Energizing Oatmeal","Breakfast Bagel","Granola with Fresh Fruit"};
 
     @BindView(R.id.listView)
     ListView mListView;
@@ -19,5 +21,8 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         ButterKnife.bind(this);
+
+        final RecipesAdapter adapter = new RecipesAdapter(this, R.layout.support_simple_spinner_dropdown_item,recipes);
+        mListView.setAdapter(adapter);
     }
 }
