9177241bfd7539fbdb7ec3cce1d500213291772e
liciolentimo <lentimo@gmail.com>
2019-03-06 09:52:01 +0300

    Added baseurl and query parameter

--- app/src/main/java/com/example/licio/moringaeats/Constants.java
+++ app/src/main/java/com/example/licio/moringaeats/Constants.java
@@ -2,4 +2,6 @@ package com.example.licio.moringaeats;
 
 public class Constants {
     public static final String YUMMLY_TOKEN = BuildConfig.YUMMLY_TOKEN;
+    public static final String YUMMLY_BASE_URL = "http://api.yummly.com/v1/api/recipes";
+    public static final String YUMMLY_INGREDIENT_QUERY_PARAMETER = "ingredient";
 }
