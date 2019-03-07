9d059504edca309db43817bf15d6ac67df07e723
liciolentimo <lentimo@gmail.com>
2019-03-07 11:26:29 +0300

    Added RecipeDetailActivity and Fragment

---
+++ app/src/main/java/com/example/licio/moringaeats/ui/RecipeDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.licio.moringaeats.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.licio.moringaeats.R;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+    }
+}
