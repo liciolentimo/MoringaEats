fcba552fd9a2376f795b24d3a2f346b5b471b1e2
liciolentimo <lentimo@gmail.com>
2019-03-13 13:42:31 +0300

    Added authentication for sign up form

--- app/src/main/java/com/example/licio/moringaeats/ui/CreateAccount.java
+++ app/src/main/java/com/example/licio/moringaeats/ui/CreateAccount.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,6 +27,8 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private ProgressDialog mAuthProgressDialog;
+    private String mName;
+    public static final String TAG = CreateAccount.class.getSimpleName();
 
     @BindView(R.id.txtName)
     EditText mTxtName;
@@ -115,6 +118,15 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
         final String email = mTxtEmail.getText().toString().trim();
         String password = mTxtPassword.getText().toString().trim();
 
+        mName = mTxtLoginText.getText().toString().trim();
+
+
+        boolean validEmail = isValidEmail(email);
+        boolean validName = isValidName(mName);
+        boolean validPassword = isValidPassword(password);
+        if (!validEmail || !validName || !validPassword) return;
+
+
         mAuthProgressDialog.show();
 
         mAuth.createUserWithEmailAndPassword(email, password)
@@ -122,15 +134,63 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         mAuthProgressDialog.dismiss();
-//                        if (task.isSuccessful()) {
-//                            Log.d(TAG, "Authentication successful");
-//                            createFirebaseUserProfile(task.getResult().getUser());
-//                        } else {
-//                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
-//                                    Toast.LENGTH_SHORT).show();
-//                        }
+                        if (task.isSuccessful()) {
+
+                            createFirebaseUserProfile(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(CreateAccount.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
 
     }
-}
+
+    private void createFirebaseUserProfile(final FirebaseUser user) {
+
+        UserProfileChangeRequest addProfileName = new UserProfileChangeRequest.Builder()
+                .setDisplayName(mName)
+                .build();
+
+        user.updateProfile(addProfileName)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, user.getDisplayName());
+                        }
+                    }
+
+                });
+    }
+
+    private boolean isValidEmail(String email) {
+            boolean isGoodEmail =
+                    (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+            if (!isGoodEmail) {
+                mTxtEmail.setError("Please enter a valid email address");
+                return false;
+            }
+            return isGoodEmail;
+        }
+
+        private boolean isValidName(String name) {
+            if (name.equals("")) {
+                mTxtName.setError("Please enter your name");
+                return false;
+            }
+            return true;
+        }
+
+        private boolean isValidPassword(String password) {
+            if (password.length() < 6) {
+                mTxtPassword.setError("Please create a password containing at least 6 characters");
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+
