9fe454484be966c214091ff2eecc584da643f44d
liciolentimo <lentimo@gmail.com>
2019-03-13 12:04:03 +0300

    Added click event to login page

--- app/src/main/java/com/example/licio/moringaeats/adapters/RecipesAdapter.java
+++ app/src/main/java/com/example/licio/moringaeats/adapters/RecipesAdapter.java
@@ -1,4 +1,4 @@
-package com.example.licio.moringaeats;
+package com.example.licio.moringaeats.adapters;
 
 import android.content.Context;
 import android.widget.ArrayAdapter;
--- app/src/main/java/com/example/licio/moringaeats/ui/CreateAccount.java
+++ app/src/main/java/com/example/licio/moringaeats/ui/CreateAccount.java
@@ -1,15 +1,41 @@
 package com.example.licio.moringaeats.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.licio.moringaeats.R;
 
-public class CreateAccount extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateAccount extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.txtName)
+    EditText mTxtName;
+    @BindView(R.id.txtEmail)EditText mTxtEmail;
+    @BindView(R.id.txtPassword)EditText mTxtPassword;
+    @BindView(R.id.btnSignUp)Button mBtnSignUp;
+    @BindView(R.id.txtLoginText)TextView mTxtLoginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        ButterKnife.bind(this);
+
+        mTxtLoginText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mTxtLoginText){
+            Intent intent = new Intent(CreateAccount.this,LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
