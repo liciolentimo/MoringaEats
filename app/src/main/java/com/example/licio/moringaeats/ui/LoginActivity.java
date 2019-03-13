1a36d5d8d85a815a97e681c31f5d6c2c3613b65c
liciolentimo <lentimo@gmail.com>
2019-03-13 11:48:25 +0300

    Added login layout

---
+++ app/src/main/java/com/example/licio/moringaeats/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.licio.moringaeats.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.licio.moringaeats.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
