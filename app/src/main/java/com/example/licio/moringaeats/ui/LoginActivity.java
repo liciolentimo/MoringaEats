d1dd81899fc425f2a314c8550f41303f4ecff5b8
liciolentimo <lentimo@gmail.com>
2019-03-13 14:03:35 +0300

    Added login functionality

--- app/src/main/java/com/example/licio/moringaeats/ui/LoginActivity.java
+++ app/src/main/java/com/example/licio/moringaeats/ui/LoginActivity.java
@@ -1,24 +1,43 @@
 package com.example.licio.moringaeats.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.licio.moringaeats.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @BindView(R.id.txtEmail)EditText mTxtEmail;
-    @BindView(R.id.txtPassword)EditText mTxtPassword;
-    @BindView(R.id.btnLogin)Button mBtnSignUp;
-    @BindView(R.id.txtSignUpText)TextView mTxtSignUpText;
+    @BindView(R.id.txtEmail)
+    EditText mTxtEmail;
+    @BindView(R.id.txtPassword)
+    EditText mTxtPassword;
+    @BindView(R.id.btnLogin)
+    Button mBtnLogin;
+    @BindView(R.id.txtSignUpText)
+    TextView mTxtSignUpText;
+
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
 
 
     @Override
@@ -28,14 +47,85 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         ButterKnife.bind(this);
 
+        createAuthProgressDialog();
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, Welcome.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+
         mTxtSignUpText.setOnClickListener(this);
+        mBtnLogin.setOnClickListener(this);
+    }
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
     public void onClick(View view) {
-        if(view == mTxtSignUpText){
-            Intent intent = new Intent(LoginActivity.this,CreateAccount.class);
+        if (view == mTxtSignUpText) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccount.class);
             startActivity(intent);
         }
+        if (view == mBtnLogin) {
+            loginwithPassword();
+        }
     }
+
+    private void loginwithPassword() {
+        String email = mTxtEmail.getText().toString().trim();
+        String password = mTxtPassword.getText().toString().trim();
+        if (email.equals("")) {
+            mTxtEmail.setError("Please enter your email");
+            return;
+        }
+        mAuthProgressDialog.show();
+        if (password.equals("")) {
+            mTxtPassword.setError("Password cannot be blank");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuthProgressDialog.dismiss();
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    });
+                }
 }
