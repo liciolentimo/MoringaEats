e0505d009e1259288f9d7d1bf91767eaab1cc3e7
liciolentimo <lentimo@gmail.com>
2019-03-14 10:52:15 +0300

    Added class to save recipes

--- app/src/main/java/com/example/licio/moringaeats/ui/ViewRecipes.java
+++ app/src/main/java/com/example/licio/moringaeats/ui/ViewRecipes.java
@@ -2,14 +2,66 @@ package com.example.licio.moringaeats.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.example.licio.moringaeats.Constants;
 import com.example.licio.moringaeats.R;
+import com.example.licio.moringaeats.adapters.FirebaseViewHolder;
+import com.example.licio.moringaeats.models.Recipe;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ViewRecipes extends AppCompatActivity {
+    private DatabaseReference mRestaurantReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_recipes);
+        setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        mRestaurantReference = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_RECIPES)
+                .child(uid);
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Recipe, FirebaseViewHolder>
+                (Recipe.class, R.layout.recipe_list_item, FirebaseViewHolder.class,
+                        mRestaurantReference) {
+
+            @Override
+            protected void populateViewHolder(FirebaseViewHolder viewHolder,
+                                              Recipe model, int position) {
+                viewHolder.onBindRecipe(model);
+
+            }
+        };
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
     }
 }
